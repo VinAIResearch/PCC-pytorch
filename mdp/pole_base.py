e09094290dca112ea769f6b064d4e3c534b89643
Tung <tungnd13@sdc2-hanoi.vinai.io-pp>
2020-01-15 15:20:51 +0700

    change from uniform noise to gaussian noise

--- mdp/pole_base.py
+++ mdp/pole_base.py
@@ -41,7 +41,7 @@ class PoleBase(object):
     def transition_function(self, s, u): # compute next state and add noise
         s_next = self.take_step(s, u)
         # add noise
-        s_next += self.noise * np.random.rand(*s_next.shape)
+        s_next += self.noise * np.random.randn(*s_next.shape)
         return s_next
 
     def render(self, s):
