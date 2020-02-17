469956e83a6f76c4d53407e9051fd130913802c7
tungnd1705 <ductungnguyen1997@gmail.com>
2020-02-17 12:26:17 +0700

    clean up the code

--- losses.py
+++ losses.py
@@ -12,11 +12,6 @@ def bernoulli(x, p):
     log_p_x = torch.mean(log_p_x)
     return log_p_x
 
-#def bernoulli(x, p):
-    #log_p_x = p.log_prob(x)
-    #log_p_x = torch.mean(log_p_x)
-    #return log_p_x
-
 def KL(normal_1, normal_2):
     kl = kl_divergence(normal_1, normal_2)
     kl = torch.mean(kl)
@@ -42,6 +37,7 @@ def ae_loss(x, p_x):
     recon_loss = -bernoulli(x, p_x)
     return recon_loss
 
+
 def curvature(model, z, u, delta, armotized):
     z_alias = z.detach().requires_grad_(True)
     u_alias = u.detach().requires_grad_(True)
@@ -51,73 +47,26 @@ def curvature(model, z, u, delta, armotized):
     z_bar = z_alias + eps_z
     u_bar = u_alias + eps_u
 
-    f_z_bar, A_bar, B_bar = model.transition(z_bar, u_bar)
+    f_z_bar, A, B = model.transition(z_bar, u_bar)
     f_z_bar = f_z_bar.mean
     f_z, A, B = model.transition(z_alias, u_alias)
     f_z = f_z.mean
 
+    z_dim, u_dim = z.size(1), u.size(1)
     if not armotized:
-        # grad_z, grad_u = torch.autograd.grad(f_z, [z_alias, u_alias], grad_outputs=[eps_z, eps_u], retain_graph=True, create_graph=True)
-        # taylor_error = f_z_bar - (grad_z + grad_u) - f_z
-        grad_z, = torch.autograd.grad(f_z, z_alias, grad_outputs=eps_z, retain_graph=True, create_graph=True)
-        taylor_error = f_z_bar - grad_z - f_z
-        cur_loss = torch.mean(torch.sum(taylor_error.pow(2), dim = 1))
+        _, B = get_jacobian(model.dynamics, z_alias, u_alias)
     else:
-        z_dim, u_dim = z.size(1), u.size(1)
-        A_bar = A_bar.view(-1, z_dim, z_dim)
-        B_bar = B_bar.view(-1, z_dim, u_dim)
-        eps_z = eps_z.view(-1, z_dim, 1)
-        eps_u = eps_u.view(-1, u_dim, 1)
-        taylor_error = f_z_bar - (torch.bmm(A_bar, eps_z).squeeze() + torch.bmm(B_bar, eps_u).squeeze()) - f_z
-        cur_loss = torch.mean(torch.sum(taylor_error.pow(2), dim = 1))
+        A = A.view(-1, z_dim, z_dim)
+        B = B.view(-1, z_dim, u_dim)
+
+    grad_z, = torch.autograd.grad(f_z, z_alias, grad_outputs=eps_z, retain_graph=True, create_graph=True)
+    grad_u = torch.bmm(B, eps_u.view(-1, u_dim, 1)).squeeze()
+    taylor_error = f_z_bar - (grad_z + grad_u) - f_z
+    cur_loss = torch.mean(torch.sum(taylor_error.pow(2), dim=1))
     return cur_loss
 
-# def curvature(model, z, u, delta, armotized):
-#     z_alias = z.detach().requires_grad_(True)
-#     u_alias = u.detach().requires_grad_(True)
-#     eps_z = torch.normal(mean=torch.zeros_like(z), std=torch.empty_like(z).fill_(delta))
-#     eps_u = torch.normal(mean=torch.zeros_like(u), std=torch.empty_like(u).fill_(delta))
-#
-#     z_bar = z_alias + eps_z
-#     u_bar = u_alias + eps_u
-#
-#     f_z_bar, A, B = model.transition(z_bar, u_bar)
-#     f_z_bar = f_z_bar.mean
-#     f_z, A, B = model.transition(z_alias, u_alias)
-#     f_z = f_z.mean
-#
-#     z_dim, u_dim = z.size(1), u.size(1)
-#     if not armotized:
-#         A, B = jacobian(model.dynamics, z_alias, u_alias)
-#     else:
-#         A = A.view(-1, z_dim, z_dim)
-#         B = B.view(-1, z_dim, u_dim)
-#
-#     eps_z = eps_z.view(-1, z_dim, 1)
-#     eps_u = eps_u.view(-1, u_dim, 1)
-#     taylor_error = f_z_bar - (torch.bmm(A, eps_z).squeeze() + torch.bmm(B, eps_u).squeeze()) - f_z
-#     cur_loss = torch.mean(torch.sum(taylor_error.pow(2), dim=1))
-#     return cur_loss * 100
 
-
-def new_curvature(model, z, u):
-    z_next, _, _, = model.dynamics(z, u)
-    z_next = z_next.mean
-    temp_z = z - z.mean(dim=0)
-    temp_z_next = z_next - z_next.mean(dim=0)
-    temp_u = u - u.mean(dim=0)
-
-    cov_z_z_next = torch.sum(temp_z * temp_z_next) ** 2
-    var_prod_z_z_next = torch.sum(temp_z ** 2) * torch.sum(temp_z_next ** 2)
-
-    # cov_u_z_next = torch.sum(temp_u * temp_z_next)**2
-    # var_prod_u_z_next = torch.sum(temp_u ** 2) * torch.sum(temp_z_next ** 2)
-    # print ('z next u: ' + str(cov_z_z_next / var_prod_z_z_next))
-    # return - cov_z_z_next / var_prod_z_z_next - cov_u_z_next / var_prod_u_z_next
-    return - cov_z_z_next / var_prod_z_z_next
-
-
-def jacobian(dynamics, batched_z, batched_u):
+def get_jacobian(dynamics, batched_z, batched_u):
     """
     compute the jacobian of F(z,u) w.r.t z, u
     """
